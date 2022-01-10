2952423c2b1808c3c64140973ad45997547ba11f
pierre <pierre@c8.unog.un.org>
2022-01-10 11:26:35 +0100

    Adding a storage stack

--- stacks/index.js
+++ stacks/index.js
@@ -1,10 +1,17 @@
 import StorageStack from "./StorageStack";
+import ApiStack from "./ApiStack";
 
 export default function main(app) {
+
   // Set default runtime for all functions
   app.setDefaultFunctionProps({
     runtime: "nodejs12.x"
   });
 
-  new StorageStack(app, "storage");
+  const storageStack = new StorageStack(app, "storage");
+
+  new ApiStack(app, "api", {
+    table: storageStack.table,
+  });
+
 }
